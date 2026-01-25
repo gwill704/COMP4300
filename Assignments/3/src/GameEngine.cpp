586f7a3a862c354a6eb56d742d48075660a0873c
Guillem <guiarfe@ific.uv.es>
2026-01-25 13:48:31 +0100

    A3: Game engine implemented some basic functionality, now with changeScene, have to implement the name of scenes

--- Assignments/3/src/GameEngine.cpp
+++ Assignments/3/src/GameEngine.cpp
@@ -1,27 +1,47 @@
 #include "GameEngine.h"
 
-void GameEngine::init(const std::string& config)
+void GameEngine::init()
 {
-    
+    m_window.create(sf::VideoMode({1280, 720}), "Assignment 3");
+    m_window.setFramerateLimit(60);
+}
+
+void GameEngine::update()
+{
+    m_scenes[m_currentScene]->update();
+}
+
+void GameEngine::quit()
+{
+    m_running = false;
 }
 
 
+template <typename T>
+void changeScene()
 {
-    std::map<const std::string&, Scene>     m_scenes;
-    sf::Window                              m_window;
-    Assets                                  m_assets;
-    const std::string&                      m_scene;
-    bool                                    m_running;
+    T newScene();
+    m_currentScene = newScene.name();
+    m_scenes[m_currentScene] = std::make_shared<T>();
 
-    void init(const std::string& config);
-    void update();
-    void quit();
+}
+
+{
+    std::map<const std::string&, std::shared_ptr<Scene>>     m_scenes;
+    sf::RenderWindow                                         m_window;
+    Assets                                                   m_assets;
+    const std::string&                                       m_currentScene;
+    bool                                                     m_running;
+
+    //void init();
+    //void update();
+    //void quit();
     
     template <typename T>
     void changeScene();
 
     Assets& getAssets();
-    sf::Window& window();
+    sf::RenderWindow& window();
     void sUserInput();
 
 public:
--- Assignments/3/src/GameEngine.h
+++ Assignments/3/src/GameEngine.h
@@ -12,11 +12,11 @@
 
 class GameEngine
 {
-    std::map<const std::string&, Scene>     m_scenes;
-    sf::Window                              m_window;
-    Assets                                  m_assets;
-    const std::string&                      m_scene;
-    bool                                    m_running;
+    std::map<const std::string&, std::shared_ptr<Scene>>     m_scenes;
+    sf::RenderWindow                                         m_window;
+    Assets                                                   m_assets;
+    const std::string&                                       m_currentScene;
+    bool                                                     m_running;
 
     void init(const std::string& config);
     void update();
@@ -26,7 +26,7 @@ class GameEngine
     void changeScene();
 
     Assets& getAssets();
-    sf::Window& window();
+    sf::RenderWindow& window();
     void sUserInput();
 
 public:
