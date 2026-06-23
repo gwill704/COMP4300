e3d13e934e7c9c596f59486cd9c24a71cc0c3328
gwill704 <gwill704@gmail.com>
2026-06-24 00:56:38 +0200

    Fix: Repeat x in size of block bounding box of previous commit

--- Assignments/3/src/Scene_Play.cpp
+++ Assignments/3/src/Scene_Play.cpp
@@ -71,8 +71,8 @@ void Scene_Play::loadLevel(const std::string& levelPath)
     block->add<CAnimation>(Assets::Instance().getAnimation("Block"), true);
     block->add<CTransform>(Vec2f(224, 480));
     // add a bounding box, this will now show up if we press the 'C' key
-    block->add<CBoundingBox>(Vec2f(Assets::Instance().getAnimation("Block").getRect().size.x,
-                                   Assets::Instance().getAnimation("Block").getRect().size.y));
+    block->add<CBoundingBox>(Vec2f(block->get<CAnimation>.animation.getRect().size.x,
+                                   block->get<CAnimation>.animation.getRect().size.y));
 
     auto question = m_entityManager.addEntity("tile");
     question->add<CAnimation>(Assets::Instance().getAnimation("Question"), true);
@@ -101,8 +101,7 @@ void Scene_Play::spawnPlayer()
     // here is a sample player entity which you can use to construct other entities
     m_player->add<CAnimation>(Assets::Instance().getAnimation("Stand"), true);
     m_player->add<CTransform>(Vec2f(224, 352)); 
-    m_player->add<CBoundingBox>(Vec2f(m_player->get<CAnimation>().animation.getRect().size.x,
-                                      m_player->get<CAnimation>().animation.getRect().size.y));
+    m_player->add<CBoundingBox>(Vec2f(48,48));
     m_player->add<CState>("stand");
     m_player->add<CInput>();
 
